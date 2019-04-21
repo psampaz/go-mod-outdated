e9c26d93e82affca41a1159446ca487921372328
psampaz <psampaz@gmail.com>
2019-04-21 22:01:27 +0300

    gofmt

--- mod/mod.go
+++ mod/mod.go
@@ -5,23 +5,24 @@ import "time"
 // Module holds information about a specifc module listed by go list
 
 type Module struct {
-	Path      string        `json:",omitempty"` // module path
-	Version   string        `json:",omitempty"` // module version
-	Versions  []string      `json:",omitempty"` // available module versions
-	Replace   *Module `json:",omitempty"` // replaced by this module
-	Time      *time.Time    `json:",omitempty"` // time version was created
-	Update    *Module `json:",omitempty"` // available update (with -u)
-	Main      bool          `json:",omitempty"` // is this the main module?
-	Indirect  bool          `json:",omitempty"` // module is only indirectly needed by main module
-	Dir       string        `json:",omitempty"` // directory holding local copy of files, if any
-	GoMod     string        `json:",omitempty"` // path to go.mod file describing module, if any
-	Error     *ModuleError  `json:",omitempty"` // error loading module
-	GoVersion string        `json:",omitempty"` // go version used in module
+	Path      string       `json:",omitempty"` // module path
+	Version   string       `json:",omitempty"` // module version
+	Versions  []string     `json:",omitempty"` // available module versions
+	Replace   *Module      `json:",omitempty"` // replaced by this module
+	Time      *time.Time   `json:",omitempty"` // time version was created
+	Update    *Module      `json:",omitempty"` // available update (with -u)
+	Main      bool         `json:",omitempty"` // is this the main module?
+	Indirect  bool         `json:",omitempty"` // module is only indirectly needed by main module
+	Dir       string       `json:",omitempty"` // directory holding local copy of files, if any
+	GoMod     string       `json:",omitempty"` // path to go.mod file describing module, if any
+	Error     *ModuleError `json:",omitempty"` // error loading module
+	GoVersion string       `json:",omitempty"` // go version used in module
 }
 
 type ModuleError struct {
 	Err string // error text
 }
+
 // FilterModules filters the list of modules provided by the go list command
 func FilterModules(modules []Module, hasUpdate, isDirect bool) []Module {
 	out := make([]Module, 0)
